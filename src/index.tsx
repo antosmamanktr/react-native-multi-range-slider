c6c9c9733985b964c6ebe99a8f3e53d4ea37dfbb
Antos Maman <antosmamanktr@gmail.com>
2026-02-25 12:18:05 +0530

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const MultiRangeSlider = () => {
+  return (
+    <View>
+      <Text>Multi Range Slider</Text>
+    </View>
+  );
+};
