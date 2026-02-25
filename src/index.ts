5d07bd8c568bd80d87573eb681253c7e4bf2281a
Antos Maman <antosmamanktr@gmail.com>
2026-02-25 12:57:30 +0530

    Commit - React Native Multi Range Slider

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export { MultiRangeSlider } from './MultiRangeSlider';
+import MultiRangeSlider from './MultiRangeSlider';
+
+export default MultiRangeSlider;
+export { MultiRangeSlider };
+export type { MultiRangeSliderProps } from './types';
